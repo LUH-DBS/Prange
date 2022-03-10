750ec8eb77f4e8e171351e66c21fd9f9c26a465a
Janek Prange <janek.prange@stud.uni-hannover.de>
2022-03-10 21:03:40 +0100

    main.py converts a single gittable to csv

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from multiprocessing import connection
 import os
 from dotenv import load_dotenv
 import psycopg2
@@ -10,13 +11,45 @@ db_params = {
     'password': os.getenv('DB_PASSWORD'),
 }
 
-with psycopg2.connect(**db_params) as connection:
+
+def main():
+    connection = psycopg2.connect(**db_params)
 
     cursor = connection.cursor()
 
-    query = "SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'"
+    table = get_gittable(cursor, 989)
 
-    outputquery = "COPY ({0}) TO STDOUT WITH CSV HEADER".format(query)
+    import pandas as pd
+    import numpy as np
+    arr = np.asarray(table)
+    pd.DataFrame(arr).to_csv('test.csv', header=None, index=False)
 
-    with open('./test.csv', 'w') as outfile:
-        cursor.copy_expert(outputquery, outfile)
+    connection.close()
+
+
+def get_gittable(cursor, tableid):
+    table = []
+    table.append(get_gittable_columninfo(cursor, tableid))
+    query = "SELECT count(distinct rowid) FROM gittables_main_tokenized WHERE tableid = %s"
+    cursor.execute(query, (tableid,))
+    row_count = cursor.fetchone()[0]
+
+    # query = "SELECT count(distinct columnid) FROM gittables_main_tokenized WHERE tableid = %s"
+    # cursor.execute(query, (tableid,))
+    # column_count = cursor.fetchone()[0]
+
+    query = "SELECT tokenized FROM gittables_main_tokenized WHERE tableid = %s and rowid = %s"
+    for rowid in range(0, row_count):
+        cursor.execute(query, (tableid, rowid))
+        table.append([r[0] for r in cursor.fetchall()])
+    return table
+
+
+def get_gittable_columninfo(cursor, tableid):
+    query = "SELECT header FROM gittables_columns_info WHERE tableid = %s"
+    cursor.execute(query, (tableid,))
+    return [r[0] for r in cursor.fetchall()]
+
+
+if __name__ == '__main__':
+    main()
