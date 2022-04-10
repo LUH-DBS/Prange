93721c02674c9803e9f9e404ce6c0401d1ad7665
Janek Prange <janek.prange@stud.uni-hannover.de>
2022-04-10 13:04:24 +0200

    changed opendata interface to use pandas

--- src/datasets/opendata.py
+++ src/datasets/opendata.py
@@ -1,12 +1,14 @@
 from ._base import Baseclass
+import pandas.io.sql as sqlio
+import pandas
 
 
 class OpenData(Baseclass):
 
-    def __init__(self, cursor) -> None:
-        self.cursor = cursor
+    def __init__(self, connection) -> None:
+        self.connection = connection
 
-    def get_table(self, tableid: int, max_rows: int, flipped=False) -> list[list]:
+    def get_table(self, tableid: int, max_rows: int, flipped=False) -> pandas.DataFrame:
         """Return a complete table from the open data tables with the id [tableid].
 
         Args:
@@ -17,24 +19,27 @@ class OpenData(Baseclass):
         Returns:
             list[list]: the table in a list format
         """
-        table = []
+        table = pandas.DataFrame([])
         query = "SELECT COUNT(DISTINCT rowid), COUNT(DISTINCT colid) FROM open_data_main_tokenized WHERE tableid = %s"
-        self.cursor.execute(query, (tableid,))
-        row_count, column_count = self.cursor.fetchone()
+        result = sqlio.read_sql_query(
+            query, self.connection, params=(tableid,))
+        row_count, column_count = result.values[0]
 
         if flipped:
             query = "SELECT term FROM open_data_main_tokenized WHERE tableid = %s and colid = %s"
             for columnid in range(0, column_count):
-                self.cursor.execute(query, (tableid, columnid))
-                table.append([r[0] for r in self.cursor.fetchall()])
+                result = sqlio.read_sql_query(
+                    query, self.connection, params=(tableid, columnid))
+                table: pandas.DataFrame = pandas.concat([table, result.T])
             return table
         else:
             if max_rows > 0:
                 row_count = min(row_count, max_rows)
             query = "SELECT term FROM open_data_main_tokenized WHERE tableid = %s and rowid = %s"
             for rowid in range(0, row_count):
-                self.cursor.execute(query, (tableid, rowid))
-                table.append([r[0] for r in self.cursor.fetchall()])
+                result = sqlio.read_sql_query(
+                    query, self.connection, params=(tableid, rowid))
+                table: pandas.DataFrame = pandas.concat([table, result.T])
             return table
 
     def pretty_columns(self, tableid: int, columnids: list[int]) -> list[str | int | list]:
@@ -53,7 +58,7 @@ class OpenData(Baseclass):
     def pretty_columns_header(self) -> list[str]:
         return ['tableid', 'columnids', 'columnnames']
 
-    def get_columnheader(self, tableid: int) -> list:
+    def get_columnheader(self, tableid: int) -> pandas.DataFrame:
         """Get the column header for a table.
 
         Args:
@@ -63,8 +68,9 @@ class OpenData(Baseclass):
             list: the header ordered by column id
         """
         query = "SELECT header FROM open_data_columns_tokenized WHERE tableid = %s ORDER BY colid"
-        self.cursor.execute(query, (tableid,))
-        return [r[0] for r in self.cursor.fetchall()]
+        result = sqlio.read_sql_query(
+            query, self.connection, params=(tableid,))
+        return result
 
     def get_columnnames(self, tableid: int, columnids: list[int]) -> list[str]:
         """Get the names of the columns
@@ -76,7 +82,7 @@ class OpenData(Baseclass):
         Returns:
             list[str]: a list with the column names
         """
-        names = self.get_columnheader(tableid)
+        names = self.get_columnheader(tableid).values[0]
         result = []
         for i in columnids:
             result.append(names[i])
