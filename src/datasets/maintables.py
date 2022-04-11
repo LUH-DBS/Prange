08599ce2ce562182cc3724902209da3b6e054db8
Janek Prange <janek.prange@stud.uni-hannover.de>
2022-04-11 18:20:16 +0200

    changed maintables interface to use pandas

--- src/datasets/maintables.py
+++ src/datasets/maintables.py
@@ -1,12 +1,14 @@
 from ._base import Baseclass
+import pandas.io.sql as sqlio
+import pandas
 
 
 class Maintables(Baseclass):
 
-    def __init__(self, cursor) -> None:
-        self.cursor = cursor
+    def __init__(self, connection) -> None:
+        self.connection = connection
 
-    def get_table(self, tableid: int, max_rows: int, flipped=False) -> list[list]:
+    def get_table(self, tableid: int, max_rows: int, flipped=False) -> pandas.DataFrame:
         """Return a complete table from the maintables with the id [tableid].
 
         Args:
@@ -17,24 +19,27 @@ class Maintables(Baseclass):
         Returns:
             list[list]: the table in a list format
         """
-        table = []
+        table = pandas.DataFrame([])
         query = "SELECT COUNT(DISTINCT colid), COUNT(DISTINCT rowid) FROM main_tokenized WHERE tableid = %s"
-        self.cursor.execute(query, (tableid,))
-        row_count, column_count = self.cursor.fetchone()
+        result = sqlio.read_sql_query(
+            query, self.connection, params=(tableid,))
+        row_count, column_count = result.values[0]
 
         if flipped:
             query = "SELECT tokenized FROM main_tokenized WHERE tableid = %s and colid = %s"
             for columnid in range(0, column_count):
-                self.cursor.execute(query, (tableid, columnid))
-                table.append([r[0] for r in self.cursor.fetchall()])
+                result = sqlio.read_sql_query(
+                    query, self.connection, params=(tableid, columnid))
+                table: pandas.DataFrame = pandas.concat([table, result.T])
             return table
         else:
             if max_rows > 0:
                 row_count = min(row_count, max_rows)
             query = "SELECT tokenized FROM main_tokenized WHERE tableid = %s and rowid = %s"
             for rowid in range(0, row_count):
-                self.cursor.execute(query, (tableid, rowid))
-                table.append([r[0] for r in self.cursor.fetchall()])
+                result = sqlio.read_sql_query(
+                    query, self.connection, params=(tableid, rowid))
+                table: pandas.DataFrame = pandas.concat([table, result.T])
             return table
 
     def pretty_columns(self, tableid: int, columnids: list[int]) -> list[str | int | list]:
