fd8838627d7cc70802fec756d85ead9bace3dfa0
Janek Prange <janek.prange@stud.uni-hannover.de>
2022-04-20 18:36:38 +0200

    column preparation of numbers in machineLearning

--- src/algorithms/machineLearning.py
+++ src/algorithms/machineLearning.py
@@ -1,8 +1,16 @@
 from ._base import Baseclass
 import pandas as pd
+from pandas.api.types import is_numeric_dtype, is_string_dtype
 
 
 class MachineLearning(Baseclass):
+    header = ["Duplicates", "Data Types", "Sorted",
+              # number
+              "Min. value", "Max. value", "Mean", "Std. Deviation",
+              # string
+              "Avg. string length", "Min. string length", "Max. string length"
+              # date?
+              ]  # 10
 
     # Possible higher efficiency with the table as a numpy array instead of a list
     def find_unique_columns(self, table: pd.DataFrame) -> pd.DataFrame:
@@ -14,13 +22,37 @@ class MachineLearning(Baseclass):
         Returns:
             pd.DataFrame: the indexes of the unique columns
         """
-        pass
+        print(self.prepare_table(table))
 
     def prepare_table(self, table: pd.DataFrame) -> pd.DataFrame:
-        result = pd.DataFrame()
-        for column in table:
-            result = pd.concat(result, self.prepare_column(column))
+        result = pd.DataFrame(columns=self.header)
+        for column_id in table:
+            result = pd.concat([result, self.prepare_column(table[column_id])])
+            # print(self.prepare_column(table[column_id]))
         return result
 
-    def prepare_column(self, column: pd.DataFrame)-> pd.DataFrame:
-        pass
+    def prepare_column(self, column: pd.DataFrame) -> pd.DataFrame:
+        # return immediatly if there are any duplicated
+        if column.duplicated().any():
+            # 10
+            return pd.DataFrame([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0]], columns=self.header)
+        # duplicate = 0, data type and sorted will be changed
+        result = [0, 0, 0]
+        # check if entries are sorted
+        if all(column[i+1] >= column[i] for i in range(0, len(column)-1)):
+            result[2] = 1
+        if all(column[i+1] <= column[i] for i in range(0, len(column)-1)):
+            result[2] = 1
+        # handle integer and float
+        if is_numeric_dtype(column):
+            result[1] = 1
+            description = column.describe()
+            result.append(description['min'])
+            result.append(description['max'])
+            result.append(description['mean'])
+            result.append(description['std'])
+            # values for strings
+            result += [0, 0, 0]
+            return pd.DataFrame([result], columns=self.header)
+        return pd.DataFrame([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]], columns=self.header)
+        raise NotImplementedError("Not implemented column type")
