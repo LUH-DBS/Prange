26bf3ded8d5b801233929bf4a038b15fcb3a45e0
Janek Prange <janek.prange@stud.uni-hannover.de>
2022-04-27 16:46:36 +0200

    updated test_auto-sklearn.py

--- src/test_auto-sklearn.py
+++ src/test_auto-sklearn.py
@@ -8,32 +8,59 @@ import sklearn.metrics
 import autosklearn.classification
 
 import pandas
-X = pandas.read_csv('src/data/training/100-999_50.csv')
-y = pandas.read_csv('src/data/training/100-999_50-result.csv')
-X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(
-    X, y, random_state=1)
 
-automl = autosklearn.classification.AutoSklearnClassifier(
-    time_left_for_this_task=120,
-    per_run_time_limit=30,
-    tmp_folder='/tmp/autosklearn_classification_example_tmp',
-)
-automl.fit(X_train, y_train, dataset_name='breast_cancer')
 
-print(automl.leaderboard())
+def main():
+    X = pandas.read_csv('src/data/training/100-999_20.csv')
+    y = pandas.read_csv('src/data/training/100-999_20-result.csv')
 
-print("\n-------------------------------\n")
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(
+        X, y, random_state=1)
 
-pprint(automl.show_models(), indent=2)
+    automl = autosklearn.classification.AutoSklearnClassifier(
+        time_left_for_this_task=120,
+        per_run_time_limit=30,
+        tmp_folder='/tmp/autosklearn_classification_example_tmp',
+    )
+    automl.fit(X_train, y_train, dataset_name="Test")
 
-print("\n-------------------------------\n")
+    # print(automl.leaderboard())
+    # print("\n-------------------------------\n")
+    # pprint(automl.show_models(), indent=2)
+    # print("\n-------------------------------\n")
 
-predictions = automl.predict(X_test)
-print("Accuracy score:", sklearn.metrics.accuracy_score(y_test, predictions))
+    predictions = automl.predict(X_test)
+    print("Accuracy score:", sklearn.metrics.accuracy_score(y_test, predictions))
 
-X2 = pandas.read_csv('src/data/training/1000-1499_50_nt.csv')
-y2 = pandas.read_csv('src/data/training/1000-1499_50_nt-result.csv')
+    X2 = pandas.read_csv('src/data/training/1000-1499_20_nt.csv').values
+    y2 = pandas.read_csv('src/data/training/1000-1499_20_nt-result.csv').values
 
-predictions = automl.predict(X2)
-print("Non trivial accuracy score:",
-      sklearn.metrics.accuracy_score(y2, predictions))
+    predictions = automl.predict(X2)
+    print("Non trivial accuracy score:",
+          sklearn.metrics.accuracy_score(y2, predictions))
+
+    save_debug_csv(X2, predictions, y2)
+
+
+def save_debug_csv(dataset, predictions, ground_truth):
+    # for row_index, (input, prediction, label) in enumerate(zip(X2, predictions, y2)):
+    #     if prediction != label:
+    #         print('Row', row_index, 'has been classified as',
+    #               prediction, 'and should be', label, '--', input)
+    debug_false_rows = pandas.DataFrame([])
+    debug_correct_rows = pandas.DataFrame([])
+    for input, prediction, label in zip(dataset, predictions, ground_truth):
+        if prediction != label:
+            # print(input, 'has been classified as', prediction, 'and should be', label)
+            debug_false_rows = pandas.concat(
+                [debug_false_rows, pandas.DataFrame(input).T])
+        else:
+            debug_correct_rows = pandas.concat(
+                [debug_correct_rows, pandas.DataFrame(input).T])
+    debug_false_rows.to_csv('debug_false_rows.csv', index=False, header=False)
+    debug_correct_rows.to_csv('debug_correct_rows.csv',
+                              index=False, header=False)
+
+
+if __name__ == "__main__":
+    main()
