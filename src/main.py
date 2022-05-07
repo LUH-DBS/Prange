ae05045a6867fc523558052c8678c48fb37fe322
Janek Prange <janek.prange@stud.uni-hannover.de>
2022-05-07 13:41:43 +0200

    updated prepare_and_train function in test module

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 import os
+from pprint import pprint
 from dotenv import load_dotenv
 import pandas as pd
 from sklearn.metrics import accuracy_score, precision_score
 from genericpath import exists
 import pickle
 
+import datasets.local as local
 from datasets.sql import Gittables, Maintables, OpenData
 import datasets.sql.csv_cache as csv_cache
 import algorithms.naiveAlgorithm as naiveAlgorithm
 import algorithms.machineLearning as machineLearning
+
+import testing
 
 load_dotenv()
 db_params = {
@@ -20,18 +24,20 @@ db_params = {
 
 
 def main():
-    if exists('test.pickle'):
-        with open('test.pickle', 'rb') as file:
-            ml = pickle.load(file)
-    else:
-        ml = machineLearning.train(
-            "src/data/training/100-999_10.csv", "test.pickle")
-    X2 = pd.read_csv('src/data/training/1000-1499_10_nt.csv').values
-    y2 = pd.read_csv('src/data/training/1000-1499_10_nt-result.csv').values
+    # if exists('test.pickle'):
+    #     with open('test.pickle', 'rb') as file:
+    #         ml = pickle.load(file)
+    # else:
+    #     ml = machineLearning.train(
+    #         "src/data/training/100-999_10.csv", "test.pickle")
+    # X2 = pd.read_csv('src/data/training/1000-1499_10_nt.csv').values
+    # y2 = pd.read_csv('src/data/training/1000-1499_10_nt-result.csv').values
 
-    predictions = ml.predict(X2)
-    print("Non trivial accuracy score:", accuracy_score(y2, predictions))
-    print("Non trivial precision score:", precision_score(y2, predictions))
+    # predictions = ml.predict(X2)
+    # print("Non trivial accuracy score:", accuracy_score(y2, predictions))
+    # print("Non trivial precision score:", precision_score(y2, predictions))
+    testing.prepare_and_train(
+        [5, 10], 100, 100, 'src/data/gittables/object_tables')
 
 
 if __name__ == '__main__':
--- src/testing/testML.py
+++ src/testing/testML.py
@@ -8,17 +8,27 @@ import datasets.local as local
 import algorithms.naiveAlgorithm as naiveAlgorithm
 import algorithms.machineLearning as machineLearning
 
+base_path_training = 'src/data/training/'
+base_path_model = 'src/data/model/'
 
-def prepare_and_train(row_count_iter: Iterable, data_path: str):
+
+def prepare_and_train(row_count_iter: Iterable, train_table_count: int, test_table_count: int, data_path: str):
     for row_count in row_count_iter:
+        training_csv_path = f'{base_path_training}{row_count}_rows/{train_table_count}_tables/{data_path.replace("src/data/", "")}/'
+        model_path = f'{base_path_model}{row_count}_rows/{train_table_count}_tables/{data_path.replace("src/data/", "")}/'
         table_iter = local.traverse_directory(data_path, row_count)
-        # machineLearning.prepare_training_iterator(
-        #     table_iter, False, 2000, 'src/data/training/')
-        train_if_not_exists(row_count, 'src/data/training/training.csv',
-                            f'src/data/model/{row_count}_rows/')
+        # training
+        machineLearning.prepare_training_iterator(
+            table_iter, False, train_table_count, training_csv_path)
+        # testing
+        machineLearning.prepare_training_iterator(
+            table_iter, True, test_table_count, training_csv_path)
+        train_if_not_exists(train_csv=training_csv_path + "training.csv",
+                            save_path=model_path,
+                            train_time=30)
 
 
-def train_if_not_exists(row_count: int, train_csv: str, save_path: str, train_time: int = 120, per_run_time: int = 30) -> AutoSklearnClassifier:
+def train_if_not_exists(train_csv: str, save_path: str, train_time: int = 120, per_run_time: int = 30) -> AutoSklearnClassifier:
     train_time_minute = int(train_time / 60)
     save_path = f"{save_path}{train_time_minute}minutes.pickle"
     if exists(save_path):
