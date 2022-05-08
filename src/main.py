53d5b89a30d779a51fbdadefdd8c4d6e5c4140aa
Janek Prange <janek.prange@stud.uni-hannover.de>
2022-05-08 13:26:44 +0200

    linted main.py

--- src/main.py
+++ src/main.py
@@ -1,17 +1,11 @@
 import os
-from pprint import pprint
 from dotenv import load_dotenv
-import pandas as pd
-from sklearn.metrics import accuracy_score, precision_score
-from genericpath import exists
-import pickle
 
 import datasets.local as local
 from datasets.sql import Gittables, Maintables, OpenData
 import datasets.sql.csv_cache as csv_cache
-import algorithms.naiveAlgorithm as naiveAlgorithm
-import algorithms.machineLearning as machineLearning
-
+import algorithms.naive_algorithm as naive_algorithm
+import algorithms.machine_learning as machine_learning
 import testing
 
 load_dotenv()
@@ -37,7 +31,7 @@ def main():
     # print("Non trivial accuracy score:", accuracy_score(y2, predictions))
     # print("Non trivial precision score:", precision_score(y2, predictions))
     testing.prepare_and_train(
-        [5, 10], 100, 100, 'src/data/gittables/object_tables')
+        [5, 10], 100, 'src/data/gittables')
 
 
 if __name__ == '__main__':
