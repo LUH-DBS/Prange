94b16f15b1c1dd06e5c1a122fed8faeba012010b
Janek Prange <janek.prange@posteo.de>
2022-07-01 22:49:18 +0200

    incorporated proof reading

--- main/table-code/proposed-method/prepare-columns.py
+++ main/table-code/proposed-method/prepare-columns.py
@@ -1,44 +1,43 @@
 if has_duplicates(column):
-  return [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    return [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 result = [0, 0, 0]
 # check if entries are sorted
 try:
-  if all(column[i+1] >= column[i] for i in range(len(column)-1)):
-      result[2] = 1
-  if all(column[i+1] <= column[i] for i in range(len(column)-1)):
-      result[2] = 1
+    if all(column[i+1] >= column[i] for i in range(len(column)-1)):
+        result[2] = 1
+    if all(column[i+1] <= column[i] for i in range(len(column)-1)):
+        result[2] = 1
 except TypeError:
-  # mostly this means the column contains None/NaN values
-  pass
+    # mostly this means the column contains None/NaN values
+    pass
 if only_bool(column):
-  result[1] = 3
-  result += [0, 0, 0, 0, 0, 0, 0]
-  return result
+    result[1] = 3
+    result += [0, 0, 0, 0, 0, 0, 0]
+    return result
 if only_numeric(column):
-  result[1] = 1
-  result += [min_value(column), max_value(column),
-             mean_value(column), std_deviation(column)]
-  # values for strings
-  result += [0, 0, 0]
-  return result
+    result[1] = 1
+    result += [min_value(column), max_value(column),
+               mean_value(column), std_deviation(column)]
+    # values for strings
+    result += [0, 0, 0]
+    return result
 # values for numbers
 result += [0, 0, 0, 0]
-try:
-    length_list = []
-    for value in column:
-        if isinstance(value, str):
-            length_list.append(len(value))
-        else:
-            raise ValueError("Not a String")
-    if len(length_list) == 0:
-        average = 0
+length_list = []
+for value in column:
+    if isinstance(value, str):
+        length_list.append(len(value))
     else:
-        average = sum(length_list)/len(length_list)
-    minimum = min(length_list)
-    maximum = max(length_list)
-    result[1] = 2
-    result += [average, minimum, maximum]
-except ValueError:
-    result[1] = 4  # mixed column, mostly None/NaN value
-    result += [0, 0, 0]
+        # mixed column, mostly None/NaN value
+        result[1] = 4
+        result += [0, 0, 0]
+        return result
+if len(length_list) == 0:
+    average = 0
+else:
+    average = sum(length_list)/len(length_list)
+minimum = min(length_list)
+maximum = max(length_list)
+result[1] = 2
+result += [average, minimum, maximum]
 return result
